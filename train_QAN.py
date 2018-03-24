4626d4c590c09f21b1399ea5d13baa9dd0566ad1
hwang1996 <37166815+hwang1996@users.noreply.github.com>
2018-03-24 16:22:52 +0800

    Update train_QAN.py
    Modify checkpoint function

--- train_QAN.py
+++ train_QAN.py
@@ -1,3 +1,4 @@
+import cv2
 import mxnet as mx
 import logging
 import dataiter_custom
@@ -44,36 +45,37 @@ def tiny_network(data):
 	conv2 = mx.sym.Convolution(data=pool1, num_filter=64, kernel=(3,3), stride=(1,1), pad=(1,1), name='conv1_sss1', no_bias=False)
 	conv3 = mx.sym.Convolution(data=conv2, num_filter=64, kernel=(3,3), stride=(1,1), pad=(1,1), name='conv2_s', no_bias=False)
 	pool2 = mx.sym.Pooling(data=conv3, pool_type="avg", kernel=(7,7), stride=(7,7), name='pool_s') #(batch_size, 64, 3, 3)
-	fc1 = mx.sym.FullyConnected(data=pool2, num_hidden=3, name='fc1_s') #(batch_size, 3)
+	fc1 = mx.sym.FullyConnected(data=pool2, num_hidden=50, name='fc1_s') #(batch_size, 3)
 	act1 = mx.sym.Activation(data=fc1, name='relu1', act_type='relu')
 	fc2 = mx.sym.FullyConnected(data=act1, num_hidden=1, name='fc2_s')
 	score = mx.sym.Activation(data=fc2, act_type='sigmoid', name='score')
-	score_l1 = mx.sym.Custom(data=score, set_num=args.set_num, per_set_num=args.per_set_num, ctx=ctx[0], 
-				op_type='set_l1_norm', name='score_l1')
+	#score_l1 = mx.sym.Custom(data=score, set_num=args.set_num, per_set_num=args.per_set_num, ctx=ctx[0], 
+							#op_type='set_l1_norm', name='score_l1')
 
-	return score_l1
+	return score
 
 def parse_args():
 	parser = argparse.ArgumentParser(description='Set-to-Set Network')
 	# training
 	parser.add_argument('--set_num', help='input set number', default=30, type=int)
 	parser.add_argument('--per_set_num', help='the number for per set', default=5, type=int)
-	parser.add_argument('--duplicate_num', help='the number of duplicate set for a batch', default=5, type=int)
+	parser.add_argument('--duplicate_num', help='the number of duplicate set for a batch', default=0, type=int)
 	parser.add_argument('--gpus', help='GPU device to train with', default='1', type=str)
+	parser.add_argument('--model_prefix', help='model prefix to save model', default='model', type=str)
 
 	args = parser.parse_args()
 	return args
 
 if __name__ == '__main__':
+	
 	logging.getLogger().setLevel(logging.DEBUG)  # logging to stdout
 	args = parse_args()
+	ctx = [mx.gpu(int(i)) for i in args.gpus.split(',')]
 
 	data_shapes = (3, 112, 96)
-	fileiter = dataiter_custom.FileIter(data_shapes=data_shapes, set_num=args.set_num, 
+	fileiter = dataiter_custom.FileIter(data_shapes=data_shapes, set_num=args.set_num, ctx=ctx[0],
 					per_set_num=args.per_set_num, duplicate_num=args.duplicate_num)
 	fileiter.reset()
-
-	ctx = [mx.gpu(int(i)) for i in args.gpus.split(',')]
 
 	label = mx.sym.var("label")
 	arg_params, aux_params, all_layers = load_model()
@@ -86,8 +88,11 @@ if __name__ == '__main__':
 	metric_cost = train_info.get_cost(5*16)
 
 	score_model = train_info.get_model(devs=ctx, network=feature_set, 
-					cost=metric_cost, num_epoch=3, lr=0.0002, wd=0.00001,
+					cost=metric_cost, num_epoch=100, lr=0.0002, wd=0.00001,
 					arg_params=arg_params, aux_params=aux_params)
+
+	model_prefix = args.model_prefix
+	checkpoint = None if model_prefix is None else mx.callback.do_checkpoint(model_prefix)
 
 	score_model.fit(X=fileiter,
 			#eval_data=val,
@@ -97,7 +102,7 @@ if __name__ == '__main__':
 			fixed_param_names=all_layers.list_arguments()[1:],
 			#kvstore=kv,
 			batch_end_callback=mx.callback.Speedometer(args.per_set_num*args.set_num, 1),
-			#epoch_end_callback=checkpoint,
+			epoch_end_callback=checkpoint,
 			eval_first=False,
 			#l2_reg=args.l2_reg,
 			#svd_epoches_period=args.do_svd_period,
