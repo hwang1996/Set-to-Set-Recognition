e9da50171ef6132d7c75c2e0e308b57ce20d57d0
hwang1996 <37166815+hwang1996@users.noreply.github.com>
2018-04-26 16:03:09 +0800

    Update custom_layer.py
    Add a new L2 penalty layer for the top N score in a batch

--- custom_layer.py
+++ custom_layer.py
@@ -1,5 +1,9 @@
 import mxnet as mx
 import numpy as np
+import time
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 class batch_pool(mx.operator.CustomOp):
 	def __init__(self, set_num, per_set_num, ctx):
@@ -9,7 +13,7 @@ class batch_pool(mx.operator.CustomOp):
 		elif isinstance(ctx, str):
 			self.set_num = int(set_num) / len(ctx.strip(',').split(','))
 		self.per_set_num = int(per_set_num)
-		self.ctx = ctx				
+		self.ctx = ctx		
 
 	def forward(self, is_train, req, in_data, out_data, aux):
 		score = in_data[0]  #batch_size x 1 <NDArray 150x1 @gpu(0)>
@@ -17,6 +21,7 @@ class batch_pool(mx.operator.CustomOp):
 		data = in_data[2]
 		#mx.nd.save('my_img', [data])
 		label = in_data[3]
+		#self.score_sum = mx.nd.sum(mx.nd.reshape(score, (self.set_num, self.per_set_num)), axis=1)
 		
 		self.score_sum = mx.nd.zeros((self.set_num, 1), ctx=score.context)
 		for i in range(self.set_num):
@@ -44,9 +49,13 @@ class batch_pool(mx.operator.CustomOp):
 	def backward(self, req, out_grad, in_data, out_data, in_grad, aux):
 		grad = mx.nd.zeros((self.per_set_num*self.set_num, 256), ctx=out_grad[0].context)
 		
+		score_sum_re = mx.nd.repeat(self.score_sum, repeats=self.per_set_num, axis=0).reshape((self.per_set_num*self.set_num,1))
+		grad = (self.feature-self.set_fea_re)/score_sum_re
+		'''
 		for i in range(self.set_num*self.per_set_num):
 			grad[i] = (self.feature[i]*self.score_sum[int(i/self.per_set_num)] \
 				- self.set_fea_re[i]*self.score_sum[int(i/self.per_set_num)])/mx.nd.square(self.score_sum[int(i/self.per_set_num)])
+		'''
 
 		grad = grad*out_grad[0]
 		grad = mx.nd.sum(grad, axis=1).reshape((self.per_set_num*self.set_num,1))
@@ -81,11 +90,70 @@ class BatchPoolProp(mx.operator.CustomOpProp):
 	def create_operator(self, ctx, shapes, dtypes):
 		return batch_pool(self.set_num, self.per_set_num, self.ctx)
 
+class l2_penalty_total(mx.operator.CustomOp):
+	def __init__(self, set_num, per_set_num, l2_param, ctx, top_n):
+		super(l2_penalty_total, self).__init__()
+		if isinstance(ctx, list):
+			self.set_num = int(set_num) / len(ctx)
+		elif isinstance(ctx, str):
+			self.set_num = int(set_num) / len(ctx.strip(',').split(','))
+		self.per_set_num = int(per_set_num)
+		self.l2_param = float(l2_param)
+		self.top_n = int(top_n)
 
+	def forward(self, is_train, req, in_data, out_data, aux):
+		self.score = in_data[0].asnumpy()
+		score_dict = {}
+		for i in range(len(self.score)):
+			score_dict[i] = self.score[i][0]
 
-class l2_penalty(mx.operator.CustomOp):
+		self.score_sorted = sorted(score_dict.items(), key = lambda x:x[1], reverse = True)
+		# import pdb; pdb.set_trace()
+		l2_penalty_result = np.zeros((self.set_num*self.per_set_num, 1))
+		for i in range(self.top_n):
+			top_position = self.score_sorted[i][0]
+			top_score = self.score_sorted[i][1]
+			#import pdb; pdb.set_trace()
+			l2_penalty_result[top_position] = self.l2_param * np.square(top_score-0.95)
+		self.assign(out_data[0], req[0], mx.nd.array(l2_penalty_result, ctx=in_data[0].context))
+
+	def backward(self, req, out_grad, in_data, out_data, in_grad, aux):
+		grad = np.zeros((self.per_set_num*self.set_num, 1))
+		for i in range(self.top_n):
+			top_position = self.score_sorted[i][0]
+			top_score = self.score_sorted[i][1]
+			# grad[top_position] = -2 * self.l2_param * np.abs(top_score-0.9)
+			grad[top_position] = 2 * self.l2_param * (top_score-0.95)
+
+		self.assign(in_grad[0], req[0], mx.nd.array(grad, ctx=in_data[0].context))
+
+@mx.operator.register("l2_penalty_total")
+class L2PenaltyTotalProp(mx.operator.CustomOpProp):
+	def __init__(self, set_num, per_set_num, l2_param, ctx, top_n):
+		super(L2PenaltyTotalProp, self).__init__(need_top_grad=True)
+		self.set_num = int(set_num)
+		self.per_set_num = int(per_set_num)
+		self.l2_param = float(l2_param)
+		self.ctx = ctx
+		self.top_n = int(top_n)
+
+	def list_arguments(self):
+		return ['score']
+
+	def list_outputs(self):
+		return ['output']
+
+	def infer_shape(self, in_shape):
+		score_shape = in_shape[0]
+		output_shape = in_shape[0]
+		return [score_shape], [output_shape], []
+
+	def create_operator(self, ctx, shapes, dtypes):
+		return l2_penalty_total(self.set_num, self.per_set_num, self.l2_param, self.ctx, self.top_n)
+
+class l2_penalty_set(mx.operator.CustomOp):
 	def __init__(self, set_num, per_set_num, l2_param, ctx):
-		super(l2_penalty, self).__init__()
+		super(l2_penalty_set, self).__init__()
 		if isinstance(ctx, list):
 			self.set_num = int(set_num) / len(ctx)
 		elif isinstance(ctx, str):
@@ -112,14 +180,15 @@ class l2_penalty(mx.operator.CustomOp):
 	def backward(self, req, out_grad, in_data, out_data, in_grad, aux):
 		grad = mx.nd.zeros((self.per_set_num*self.set_num, 1), ctx=in_data[0].context)
 		for i in range(self.set_num):
-			grad[self.max_index[i]] = -2 * self.l2_param * mx.nd.abs(self.score_max[i]-0.95)
+			# grad[self.max_index[i]] = -2 * self.l2_param * mx.nd.abs(self.score_max[i]-0.9)
+			grad[self.max_index[i]] = 2 * self.l2_param * (self.score_max[i]-0.95)
 
 		self.assign(in_grad[0], req[0], grad)
 
-@mx.operator.register("l2_penalty")
-class L2PenaltyProp(mx.operator.CustomOpProp):
+@mx.operator.register("l2_penalty_set")
+class L2PenaltySetProp(mx.operator.CustomOpProp):
 	def __init__(self, set_num, per_set_num, l2_param, ctx):
-		super(L2PenaltyProp, self).__init__(need_top_grad=True)
+		super(L2PenaltySetProp, self).__init__(need_top_grad=True)
 		self.set_num = int(set_num)
 		self.per_set_num = int(per_set_num)
 		self.l2_param = float(l2_param)
@@ -137,7 +206,7 @@ class L2PenaltyProp(mx.operator.CustomOpProp):
 		return [score_shape], [output_shape], []
 
 	def create_operator(self, ctx, shapes, dtypes):
-		return l2_penalty(self.set_num, self.per_set_num, self.l2_param, self.ctx)
+		return l2_penalty_set(self.set_num, self.per_set_num, self.l2_param, self.ctx)
 
 
 
