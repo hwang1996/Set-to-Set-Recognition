f60b4502c229c168c8004d397efda7414f416cad
hwang1996 <37166815+hwang1996@users.noreply.github.com>
2018-04-26 16:04:57 +0800

    Update dataiter_custom.py
    Add the custom process for val data

--- dataiter_custom.py
+++ dataiter_custom.py
@@ -6,10 +6,14 @@ import mxnet as mx
 import os
 import math
 from mxnet.io import DataIter, DataBatch
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 
 class FileIter(DataIter):
-	def __init__(self, data_shapes, set_num, per_set_num, duplicate_num, hdfs_path, max_times_epoch, data_name="data", label_name="label"):
+	def __init__(self, data_shapes, set_num, per_set_num, duplicate_num, hdfs_path, max_num_of_duplicate_set,\
+				rec_name, max_times_epoch, data_type, data_name="data", label_name="label"):
 		#duplicate_num <= set_num/2
 		super(FileIter, self).__init__()
 		self.batch_size = set_num*per_set_num
@@ -21,19 +25,15 @@ class FileIter(DataIter):
 		self.label_name = label_name
 		self.times_epoch = 0
 		self.max_times_epoch = max_times_epoch
-		#self.data = mx.nd.zeros((self.batch_size, self.data_shapes[0], self.data_shapes[1], self.data_shapes[2]), self.ctx)
-		#self.label = mx.nd.zeros((self.batch_size, ), self.ctx)
-		
-		self.train = mx.io.ImageRecordIter(
-			path_imgrec = hdfs_path + 'hangzhougongan_train.rec',
-			data_shape  = self.data_shapes,
-			batch_size  = self.batch_size,
-			#shuffle     = 0,                 
-			round_batch = 0,
-			#scale       = 0.007843137,
-			#mean_r      = 127.5,
-			#preprocess_threads = 2,
-		)
+		self.hdfs_path = hdfs_path
+		self.rec_name = rec_name
+		self.data_type = data_type
+		self.max_num_of_duplicate_set = max_num_of_duplicate_set
+		self.i = 0
+		self.j = 0
+		self.end = 0
+		#self.data = np.ones((self.batch_size, self.data_shapes[0], self.data_shapes[1], self.data_shapes[2]), dtype='float32')
+		#self.label = np.zeros((self.batch_size, ), dtype=np.float32)
 
 	def _shuffle(self):
 		random.shuffle(self.train_list)
@@ -46,7 +46,22 @@ class FileIter(DataIter):
 	def provide_label(self):
 		return [(self.label_name, (self.batch_size, ))]
 
+	@property
+	def batchsize(self):
+		return self.batch_size
+
 	def get_total_list(self):
+		self.train = mx.io.ImageRecordIter(
+			path_imgrec = self.hdfs_path + self.rec_name,
+			data_shape  = self.data_shapes,
+			#batch_size  = self.batch_size,
+			batch_size  = 100,
+			#shuffle     = 0,                 
+			round_batch = 0,
+			#scale       = 0.007843137,
+			#mean_r      = 127.5,
+			#preprocess_threads = 2,
+		)
 		self.train.reset()
 
 		cur_num = 0
@@ -63,12 +78,117 @@ class FileIter(DataIter):
 			#if cur_num == 10:
 			#	break
 
-		self.sorted_label_img = sorted(label_img.items(), key = lambda x:int(x[0].split(' ')[0])) #label = sorted_label_img[i][0]   img = sorted_label_img[i][1] 
-		#import pdb; pdb.set_trace()
+		sorted_label_img = sorted(label_img.items(), key = lambda x:int(x[0].split(' ')[0])) #label = sorted_label_img[i][0]   img = sorted_label_img[i][1] 
+		self.sorted_label_img = np.array(sorted_label_img, dtype=object)
+		print "total number is " + str(len(self.sorted_label_img))
+		# import pdb; pdb.set_trace()
+
+	def get_val_list(self):
+		data_iter = mx.io.ImageRecordIter(
+			path_imgrec = self.hdfs_path + self.rec_name,
+			data_shape  = (3, 112, 96),
+			batch_size  = 10,
+			shuffle     = 0,  
+			round_batch = 0,
+			scale       = 0.007843137,
+			mean_r      = 127.5,
+			preprocess_threads = 1,
+		)
+		data_iter.reset()
+		self.val_label_img = {} 
+		while data_iter.iter_next():
+			img_batch = data_iter.getdata()[0].asnumpy()
+			label_batch = data_iter.getlabel().asnumpy()
+			for i in range(len(label_batch)):
+				label = int(label_batch[i])
+				self.val_label_img.setdefault(label, [])
+				self.val_label_img[label].append(img_batch[i])
+
+	def val_next(self):
+		if self.iter_next():
+			val_data = []
+			val_label = []
+			val_num = 0
+			for i in range(self.i, len(self.val_label_img)):
+				img_data = np.array(self.val_label_img[i])
+				
+				for j in range(self.j, len(img_data)/self.per_set_num):
+					# if j==(len(img_data)/self.per_set_num-1) and len(img_data)%self.per_set_num!=0:
+					# 	self.j = 0
+					# 	break
+					# elif j==(len(img_data)/self.per_set_num-1) and len(img_data)%self.per_set_num==0:
+					# 	val_data.append(img_data[j*self.per_set_num:])
+					# 	label_tmp = np.ones((self.per_set_num, ))*i
+					# 	val_label.append(label_tmp)
+					# 	val_num += 1
+					# 	self.j = 0
+					# elif j==100:
+					# 	self.j = 0
+					# 	break
+					# else:
+					val_data.append(img_data[j*self.per_set_num:(j+1)*self.per_set_num])
+					label_tmp = np.ones((self.per_set_num, ))*i
+					val_label.append(label_tmp)
+					val_num += 1
+					if j==(len(img_data)/self.per_set_num-1):
+						self.j = 0
+					if j==30:
+						self.j = 0
+						break
+					if val_num == self.set_num:
+						self.j = j
+						break
+				if val_num == self.set_num:
+					self.i = i
+					break
+			val_data = np.concatenate(val_data)
+			val_label = np.concatenate(val_label)
+			# import pdb; pdb.set_trace()
+			
+			if val_label.shape[0] != self.set_num*self.per_set_num:
+				val_num = 0
+				lack_num = self.set_num*self.per_set_num-val_label.shape[0]
+				lack_data = []
+				lack_label = []
+				for i in range(0, len(self.val_label_img)):
+					img_data = np.array(self.val_label_img[i])
+					for j in range(0, len(img_data)/self.per_set_num):
+						lack_data.append(img_data[j*self.per_set_num:(j+1)*self.per_set_num])
+						label_tmp = np.ones((self.per_set_num, ))*i
+						lack_label.append(label_tmp)
+						val_num += 1
+						if val_num == lack_num/self.per_set_num:
+							break
+					if val_num == lack_num/self.per_set_num:
+						break
+				# import pdb; pdb.set_trace()
+				lack_data = np.concatenate(lack_data)
+				lack_label = np.concatenate(lack_label)
+				val_data = np.concatenate((val_data, lack_data))
+				val_label = np.concatenate((val_label, lack_label))
+				self.end = 1
+
+			self.data = [mx.nd.array(val_data)]
+			self.label = [mx.nd.array(val_label)]
+			return DataBatch(data=self.data, label=self.label)
+		else:
+			raise StopIteration
+
 
 	def reset(self):
-		self.get_total_list()
-		self.times_epoch = 0
+		if self.data_type == 'train':
+			if self.times_epoch == 0:
+				self.get_total_list()
+				self.get_label_idx()
+			else:
+				self.times_epoch = 0
+		elif self.data_type == 'val':
+			self.i = 0
+			self.j = 0
+			if self.end == 0:
+				self.get_val_list()
+			else:
+				self.end = 0
 		#tmp_list[i] tuple -> list
 		'''
 		self.tmp_list = []
@@ -77,14 +197,27 @@ class FileIter(DataIter):
 			#self.tmp_list[i].append(1)
 		'''
 	def iter_next(self):
-		if self.get_label_idx():
+		if self.data_type == 'train':
+			# if self.get_label_idx():
+			# 	return self.max_times_epoch > self.times_epoch
+			# else:
+			# 	return False
 			return self.max_times_epoch > self.times_epoch
-		else:
-			return False
+		elif self.data_type == 'val':
+			if self.end==1:
+				return False
+			else:
+				return True
+
+	def next(self):
+		if self.data_type == 'train':
+			return self.train_next()
+		elif self.data_type == 'val':
+			return self.val_next()
 
 	def get_label_idx(self):
 		self.label_idx = {}
-		if self.sorted_label_img:
+		if self.sorted_label_img.any():
 			tmp = int(self.sorted_label_img[0][0].split(' ')[0])  #the first one
 			self.label_idx[tmp] = 0
 			for i in range(len(self.sorted_label_img)):
@@ -97,7 +230,37 @@ class FileIter(DataIter):
 			return False
 
 	def get_train_list(self):
-		random_list = random.sample(self.label_idx.keys(), self.set_num)  #label list
+		# random_list = random.sample(self.label_idx.keys(), self.set_num)  #label list
+		# if self.duplicate_num==0:
+		# 	random_list = random_list
+		# else:
+		# 	duplicate_list = random.sample(random_list, self.duplicate_num)
+		# 	duplicate_tmp = 0
+		# 	for i in range(len(random_list)):
+		# 		if random_list[i] in duplicate_list:
+		# 			continue
+		# 		else:
+		# 			random_list[i] = duplicate_list[duplicate_tmp]
+		# 			duplicate_tmp += 1
+		# 			if duplicate_tmp == len(duplicate_list):
+		# 				break
+
+		random_list = []
+		list_tmp = random.sample(self.label_idx.keys(), self.set_num)
+		duplicate_list = set(random.sample(list_tmp, self.duplicate_num))
+		for clabel in duplicate_list:
+			dup_num = random.randint(2, self.max_num_of_duplicate_set)
+			random_list.extend([clabel] * dup_num)
+		for clabel in list_tmp:
+			if len(random_list) >= self.set_num:
+				random_list = random_list[0:self.set_num]
+				break
+			if clabel in duplicate_list:
+				continue
+			else:
+				random_list.append(clabel)
+
+		'''
 		self.duplicate_clean_position = []
 		self.duplicate_noise_position = []
 		self.others_position = []
@@ -120,7 +283,7 @@ class FileIter(DataIter):
 					elif duplicate_tmp == len(duplicate_list):
 						for j in range(self.per_set_num):
 							self.others_position.append(i*self.per_set_num+j)
-
+		'''
 		total_list = []
 		# len(self.label_idx) = 53950
 		for i in range(self.set_num):
@@ -135,8 +298,33 @@ class FileIter(DataIter):
 
 		self.train_list = total_list
 		#import pdb; pdb.set_trace()
+	
+	def save_img(self, score, nbatch, epoch):
+		random_set_num = 10
+		data = self.data[0].asnumpy()
+		#import pdb; pdb.set_trace()
+		plt.figure(num='train_img',figsize=(10,20))
+		for i in range(random_set_num):
+			for j in range(self.per_set_num):
+				#import pdb; pdb.set_trace()
+				plt.subplot(random_set_num, self.per_set_num, i*self.per_set_num+j+1)
+				score_float = '%.4f' %score[i*self.per_set_num+j][0]
+				plt.title(score_float, fontsize=20)
+				data_mean = data[i*self.per_set_num+j]*127.5
+				data_plus = data_mean
+				data_plus[0,:] = data_mean[0,:]+127.5
+				data_origin = data_plus.astype('uint8').transpose((1, 2, 0))
+				plt.imshow(data_origin)
+				plt.axis('off')
+		#plt.tight_layout(pad=1, h_pad=0.5, w_pad=0.5)
+		plt.tight_layout()
+		img_name = "epoch-"+str(epoch)+"_"+"batch-"+str(nbatch)+".png"
+		plt.savefig("epoch-"+str(epoch)+"_"+"batch-"+str(nbatch)+".png")
+		# os.execl("/usr/bin/env", "sh", "./test.sh", "epoch-"+str(epoch)+"_"+"batch-"+str(nbatch)+".png", "hdfs://hobot-bigdata/user/hao01.wang/")
+		#print "Save Image OK"
 
-	def next(self):
+
+	def train_next(self):
 		self.times_epoch += 1
 
 		if self.iter_next():
@@ -144,62 +332,68 @@ class FileIter(DataIter):
 			data = np.ones((self.batch_size, self.data_shapes[0], self.data_shapes[1], self.data_shapes[2]), dtype='float32')
 			label = np.zeros((self.batch_size, ), dtype=np.float32)
 			#self._shuffle()
-			
+			'''
 			if self.duplicate_num==0:
 				for i in range(len(self.train_list)):
 					prob = np.random.uniform()
 					if prob <= 0.30:
 						data_origin = self.sorted_label_img[self.train_list[i]][1].astype('float32').transpose((1, 2, 0))
 						kernel, anchor = motion_blur(random.randint(15, 40), random.randint(20, 60))
-						data[i] = cv2.filter2D(data_origin, -1, kernel, anchor=anchor).transpose((2, 0, 1))
+						self.data[i] = cv2.filter2D(data_origin, -1, kernel, anchor=anchor).transpose((2, 0, 1))
 					else:
-						data[i] = self.sorted_label_img[self.train_list[i]][1].astype('float32')
+						self.data[i] = self.sorted_label_img[self.train_list[i]][1].astype('float32')
 
 			else:
 				for i in range(len(self.train_list)):
 					if i in self.duplicate_clean_position:
-						data[i] = self.sorted_label_img[self.train_list[i]][1].astype('float32')
+						self.data[i] = self.sorted_label_img[self.train_list[i]][1].astype('float32')
 					elif i in self.duplicate_noise_position:
 						prob = np.random.uniform()
 						if prob <= 0.80:
 							data_origin = self.sorted_label_img[self.train_list[i]][1].astype('float32').transpose((1, 2, 0))
 							kernel, anchor = motion_blur(random.randint(20, 60), random.randint(20, 60))
-							data[i] = cv2.filter2D(data_origin, -1, kernel, anchor=anchor).transpose((2, 0, 1))
+							self.data[i] = cv2.filter2D(data_origin, -1, kernel, anchor=anchor).transpose((2, 0, 1))
 						else:
-							data[i] = self.sorted_label_img[self.train_list[i]][1].astype('float32')
+							self.data[i] = self.sorted_label_img[self.train_list[i]][1].astype('float32')
 					elif i in self.others_position:
 						prob = np.random.uniform()
 						if prob <= 0.30:
 							data_origin = self.sorted_label_img[self.train_list[i]][1].astype('float32').transpose((1, 2, 0))
 							kernel, anchor = motion_blur(random.randint(20, 60), random.randint(20, 60))
-							data[i] = cv2.filter2D(data_origin, -1, kernel, anchor=anchor).transpose((2, 0, 1))
+							self.data[i] = cv2.filter2D(data_origin, -1, kernel, anchor=anchor).transpose((2, 0, 1))
 						else:
-							data[i] = self.sorted_label_img[self.train_list[i]][1].astype('float32')
+							self.data[i] = self.sorted_label_img[self.train_list[i]][1].astype('float32')
 
 			occlusion_aug(self.batch_size, self.data_shapes, max_w=60, max_h=100, 
-							min_prob=0.0, max_prob=0.3, img=data)
-			
+							min_prob=0.0, max_prob=0.3, img=self.data)
+			'''
 			'''
 			for i in range(len(data)):
-				cv2.imwrite(str(i)+'.jpg', cv2.cvtColor(data[i].transpose((1, 2, 0)), cv2.COLOR_RGB2BGR))
+				cv2.imwrite('/data-sdc/hao01.wang/longhu/'+str(i)+' '+str(self.sorted_label_img[self.train_list[i]][0].split(' ')[0])+'.jpg', cv2.cvtColor(self.sorted_label_img[self.train_list[i]][1].astype('uint8').transpose((1, 2, 0)), cv2.COLOR_RGB2BGR))
 				#print i
 			import pdb; pdb.set_trace()
 			'''
 			#import pdb; pdb.set_trace()
 			
 			for i in range(self.batch_size):
-				data_aug = data[i]
+				data_aug = self.sorted_label_img[self.train_list[i]][1].astype('float32')
 				data_mean = data_aug
 				data_mean[0,:] = data_aug[0,:]-127.5
 				data_scale = data_mean/127.5
 
 				data[i] = data_scale
 				label[i] = int(self.sorted_label_img[self.train_list[i]][0].split(' ')[0])
+
+			longhu_train_data = np.load('longhu_train_data.npy')
+			longhu_train_label = np.load('longhu_train_label.npy')
+			data[0:50] = longhu_train_data
+			label[0:50] = longhu_train_label
+			#import pdb; pdb.set_trace()
 			
 			self.data = [mx.nd.array(data)]
 			self.label = [mx.nd.array(label)]
 			#import pdb; pdb.set_trace()
-			self.train_list = list(set(self.train_list))
+			#self.train_list = list(set(self.train_list))
 
 			'''
 			#delete samples which are trained already
